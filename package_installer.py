9db1d9035fc58945ed5f7e8d90549d1d4185f420
Turfader <jbouyer151@gmail.com>
2023-11-13 08:55:09 -0800

    moved package installer script to a separate file for better readability.

--- package_installer.py
+++ package_installer.py
@@ -1,4 +1,3 @@
-'''
 import sys
 import venv
 from os import getcwd, path
@@ -17,36 +16,9 @@ if not path.exists(venv_folder):
     print("Installing dependencies...")
 
     # Add pip installation names here for any new package.
-    packages = ["ntplib"]
+    packages = ["ntplib", "pygame"]
     for package in packages:
         run([sys.executable, "-m", "pip", "install", package], check=True)
         print(f"Installed package: {package}")
 
     print(f"Venv creation completed in {round(time()-start, 2)}s")
-'''
-
-print("fetching datetime")
-import package_installer
-import ntplib
-from datetime import datetime
-
-ntp_client = ntplib.NTPClient()
-ntp_server = 'time.windows.com'
-
-try:
-    response = ntp_client.request(ntp_server)
-    current_time = response.tx_time
-    current_datetime = datetime.fromtimestamp(current_time)
-    print("Current time: ", current_datetime)
-
-    print("Displaying in window")
-    import tkinter as tk
-    from tkinter import messagebox
-
-    root = tk.Tk()
-    root.withdraw()
-    messagebox.showinfo("Current Time", current_datetime)
-
-
-except Exception as e:
-    print(e)
